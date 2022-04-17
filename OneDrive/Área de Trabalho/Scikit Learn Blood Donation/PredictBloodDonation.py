21b47abf313777402a506aa6d2553bfbeae1b1e2
silas eduardo <silas.edu.bjg@gmail.com>
2022-04-17 19:52:50 -0300

    Tratando dataset e testando modelos

--- OneDrive/Área de Trabalho/Scikit Learn Blood Donation/PredictBloodDonation.py
+++ OneDrive/Área de Trabalho/Scikit Learn Blood Donation/PredictBloodDonation.py
@@ -32,3 +32,58 @@ dataset = pd.read_csv(ficheiro, names=colunas, skiprows=1, delimiter=";")
 print(dataset.shape)
 
 print(dataset.head())
+array = dataset.values
+X = array[:, 0:4].astype(float)
+Y = array[:, 4]
+test_size = 0.20
+seed = 7
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=test_size, random_state=seed)
+num_folds = 10
+scoring = 'accuracy'
+models = []
+models.append(('LR', LogisticRegression(solver='newton-cg')))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('SGDC', SGDClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+models.append(('LIR', LinearRegression()))
+
+np.random.seed(7)
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds)
+    cv_results = cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: % f ( % f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = plt.figure()
+fig.suptitle('Comparação dos Modelos')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), ('LR', LogisticRegression(solver='newton-cg'))])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsClassifier())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeClassifier())])))
+pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()),('NB', GaussianNB())])))
+pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC())])))
+pipelines.append(('ScaledSGDC', Pipeline([('Scaler', StandardScaler()),('SGDC', SGDClassifier())])))
+pipelines.append(('ScaledLIR', Pipeline([('Scaler', StandardScaler()),('LIR', LinearRegression())])))
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
